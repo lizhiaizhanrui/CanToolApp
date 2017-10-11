0511b8134f5df8152785feffe977c64e7a094579
JinMengKang <983177289@qq.com>
2017-10-11 15:17:10 +0800

    kjm2

--- CanToolApp/gen/com/example/cantoolapp/R.java
+++ CanToolApp/gen/com/example/cantoolapp/R.java
@@ -22,6 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int msgbox_rec=0x7f020001;
+        public static final int msgbox_send=0x7f020002;
     }
     public static final class id {
         public static final int LayoutSwitch=0x7f08000c;
